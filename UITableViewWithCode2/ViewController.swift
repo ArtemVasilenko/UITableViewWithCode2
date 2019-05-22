500bed4bbf9f9cdb059bf0cf44b162991260762e
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-05-22 20:11:28 +0300

    Initial Commit

---
+++ UITableViewWithCode2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableViewWithCode2
+//
+//  Created by Артем on 5/22/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
